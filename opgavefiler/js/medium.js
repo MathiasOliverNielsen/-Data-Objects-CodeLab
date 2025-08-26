9687aec2fb2f634a425a45f7cbb53a161333e43e
MathiasNielsen99 <150657702+MathiasNielsen99@users.noreply.github.com>
2025-08-26 09:59:42 +0200

    opgave 1 og 2 gul

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -1,11 +1,18 @@
 /* Opgave 1*/
 console.group('opgave 1');
+const data = {
+  items: ['a', 'b', 'c', 'd', 'e'],
+};
 
-console.groupEnd();
+data.items.push('f');
+
+console.log(data.items);
 
 /*opgave 2*/
 console.group('opgave 2');
-
+// Slet item 2 fra data.items arrayen
+data.items.splice(1, 1);
+console.log(data.items);
 console.groupEnd();
 
 /*opgave 3*/
