51cf6568fbbf9c17364fb0f62fd6fcfa5cf87d81
MathiasNielsen99 <150657702+MathiasNielsen99@users.noreply.github.com>
2025-08-26 09:52:11 +0200

    prettier reset

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,18 +1,14 @@
 /* opgave 1 */
-console.group("opgave 1");
+console.group('opgave 1');
 
 console.groupEnd();
 
 /* opgave 2 */
-console.group("opgave 2");
-
-
+console.group('opgave 2');
 
 console.groupEnd();
 
 /* opgave 3 */
-console.group("opgave 3");
-
-
+console.group('opgave 3');
 
 console.groupEnd();
--- opgavefiler/js/hard.js
+++ opgavefiler/js/hard.js
@@ -1,16 +1,12 @@
 /* opgave 1 */
 
-
-
-console.group("opgave 1");
+console.group('opgave 1');
 
 console.groupEnd();
 /* opgave 2 */
-console.group("opgave 2");
-
-
+console.group('opgave 2');
 
 console.groupEnd();
 /* opgave 3 */
-console.group("opgave 3");
+console.group('opgave 3');
 console.groupEnd();
