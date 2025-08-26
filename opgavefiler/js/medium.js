27f1c74a6fdc7accea05c7506709c1ce9ca4cab6
MathiasNielsen99 <150657702+MathiasNielsen99@users.noreply.github.com>
2025-08-26 10:01:05 +0200

    opgave 3 gul

--- opgavefiler/js/medium.js
+++ opgavefiler/js/medium.js
@@ -7,15 +7,16 @@ const data = {
 data.items.push('f');
 
 console.log(data.items);
-
+console.groupEnd();
 /*opgave 2*/
 console.group('opgave 2');
 // Slet item 2 fra data.items arrayen
 data.items.splice(1, 1);
 console.log(data.items);
 console.groupEnd();
-
 /*opgave 3*/
 console.group('opgave 3');
-
+// Skift item 3 til "changed" i data.items
+data.items[2] = 'changed';
+console.log(data.items);
 console.groupEnd();
